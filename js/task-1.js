62401e01be2c52be029e3585a00d521eb29664c1
didukhroma <70459354+didukhroma@users.noreply.github.com>
2020-11-25 22:46:41 +0200

    task-2

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,14 @@
 // константа name
 const name = 'Генератор защитного поля';
-//шаблонная строка
-const message = `Выбран «${name}», цена за штуку ${price} кредитов`;
+
 //переменная price
 let price = 1000;
+
 //вивод в консоль
-console.log(message);
+console.log(`Выбран «${name}», цена за штуку ${price} кредитов`);
+
 //новое значение переменной price
 price = 2000;
+
 //вивод в консоль
-console.log(message);
+console.log(`Выбран «${name}», цена за штуку ${price} кредитов`);
